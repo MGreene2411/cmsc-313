94a3414456d4eba0803c212cfd2d64871b0c95a3
Michael Greene <mgreene3@umbc.edu>
2025-05-07 20:06:05 -0400

    Added label for name and class

--- Homework9/myProgram.asm
+++ Homework9/myProgram.asm
@@ -3,18 +3,32 @@
 ; Class: CMSC 313 (Mo/Wed : 10-11:15AM)
 
 SECTION .data
-msg     db      'Hello from Michael Greene!', 0Ah
+introMsg    db  'Michael Greene - CMSC 313 - Spring 2025', 0Ah
+introLen    equ $ - introMsg
+
+msg         db  'Hello from Michael Greene!', 0Ah
+msgLen      equ $ - msg
 
 SECTION .text
-global  _start
+global _start
 
 _start:
-    mov     edx, 27        ; Length of message
-    mov     ecx, msg       ; Message address
-    mov     ebx, 1         ; STDOUT
-    mov     eax, 4         ; sys_write
+
+    ; Print intro message
+    mov     edx, introLen     ; length of intro
+    mov     ecx, introMsg     ; pointer to intro message
+    mov     ebx, 1            ; file descriptor (stdout)
+    mov     eax, 4            ; syscall number for sys_write
     int     80h
 
-    mov     ebx, 0         ; Exit code 0 (no error)
-    mov     eax, 1         ; sys_exit
+    ; Print hello message
+    mov     edx, msgLen       ; length of hello message
+    mov     ecx, msg          ; pointer to message
+    mov     ebx, 1            ; stdout
+    mov     eax, 4            ; sys_write
+    int     80h
+
+    ; Exit
+    mov     ebx, 0            ; exit code
+    mov     eax, 1            ; syscall for exit
     int     80h
